2a7c9be50b715039a969c4713393b02a1fbe3812
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-16 22:02:03 -0300

    exercicio 03

--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,14 @@
 const numeros = [54, 22, 14, 87, 10, 284];
 
+let encontrado = false;
+
+for (let i = 0; i < numeros.length; i++){
+    if (numeros[i] === 10){
+        console.log(i-1);
+        encontrado= true;
+        break; 
+    }
+}
+if (encontrado === false){
+    console.log(-1);
+}
