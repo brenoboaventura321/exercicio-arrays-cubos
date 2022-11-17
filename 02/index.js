acd6af4ac824c5de0cb6f464239024692b7b0e31
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-16 21:39:56 -0300

    exercicio 02

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,16 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+
+let quantLetraE=0;
+
+for (let letra of letras ){
+    if(letra==="E" || letra=="e"){
+    quantLetraE++;
+
+    }
+}
+
+if(quantLetraE !== 0){
+    console.log(`Foram encontradas ${quantLetraE} letras "E" ou "e"`);
+}else{
+    console.log('Não foram encontradas nenhuma letra "E" ou "e" ')
+}
