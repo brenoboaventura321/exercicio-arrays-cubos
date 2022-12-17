1f8de121c9015597f42ec7c1d25352298b086b2e
Breno Boaventura <99199296+brenoboaventura321@users.noreply.github.com>
2022-12-17 16:10:10 -0300

    exercicio 07

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,10 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomesComA = [];
+
+for (const nome of nomes) {
+  if (nome[0] === "A" || nome[0] === "a") {
+    nomesComA.push(nome);
+  }
+}
+
+console.log(nomesComA)
