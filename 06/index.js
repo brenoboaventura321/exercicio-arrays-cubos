555fb2563990c40ff9f9130765fdac181e18e97e
Breno Boaventura <99199296+brenoboaventura321@users.noreply.github.com>
2022-12-17 16:05:31 -0300

    exercicio 06

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,16 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+const arrayFinal = [];
 
+for (const item of numeros) {
+  if (item % 2 === 0) {
+    arrayFinal.push(item);
+  }
+}
+
+let soma = 0;
+
+for (const item2 of arrayFinal) {
+  soma += item2;
+}
+
+console.log(soma);
