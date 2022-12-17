ebf4f67390089efb1f7920dca23fd5881ebb6021
Breno Boaventura <99199296+brenoboaventura321@users.noreply.github.com>
2022-12-17 15:59:55 -0300

    Exercicio 05

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,9 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+const arrayFinal = [];
+
+for (const item of original) {
+  if (item % 2 === 0) {
+    arrayFinal.push(item);
+  }
+}
+console.log(arrayFinal);
